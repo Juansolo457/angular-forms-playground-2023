f013888c4a5e4688e3efff6a042f52e9c037036d
JuanJ <juan.juarez@officeally.com>
2023-12-20 05:49:57 -0800

    working async validator and styling of forms

--- angular-forms-playground-2023/src/app/app.component.ts
+++ angular-forms-playground-2023/src/app/app.component.ts
@@ -1,15 +1,45 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {FormArray, FormControl, FormGroup, isFormControl, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  form = new FormGroup({
+    firstName: new FormControl('Alfred'),
+    lastName: new FormControl('Ramona'),
+    nickname: new FormControl(''),
+    email: new FormControl('juan@oa.com'),
+    yearOfBirth: new FormControl(1900),
+    passport: new FormControl(''),
+    address: new FormGroup({
+      fullAddress: new FormControl(''),
+      city: new FormControl(''),
+      postCode: new FormControl(0),
+    }),
+    phones: new FormArray([
+      new FormControl('')
+    ])
+  });
+
+  years =  this.getYears();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  private getYears() {
+    const now = new Date().getUTCFullYear();
+    return Array(now - (now - 40)).fill('').map((_, idx) => now - idx);
+  }
+
   hover: boolean = false;
 
   onMouseEnter() {
@@ -18,5 +48,13 @@ export class AppComponent {
 
   onMouseLeave() {
     this.hover = false;
+  }
+
+  addPhone() {
+    this.form.controls.phones.insert(0, new FormControl(''))
+  }
+
+  removePhone(index: number) {
+    this.form.controls.phones.removeAt(index);
   }
 }
